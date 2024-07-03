b3fde05528f7b9486300c13c6558281bda62f4e1
Arthur Santana <arthursglbr@gmail.com>
2024-07-02 21:38:49 -0300

    Initial commit with site files

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('links').innerHTML = `
+    <a class="link" href="https://github.com/gaspararthur" target="_blank">
+        <div class="link-icon" style="background-color: #333;">
+            <i class="fa-brands fa-github"></i>
+        </div>
+        <div class="link-name">
+            GitHub
+        </div>
+    </a>
+    <a class="link" href="https://www.linkedin.com/in/arthur-santana-293b7a251/" target="_blank">
+        <div class="link-icon" style="background-color: #0077B5;">
+            <i class="fa-brands fa-linkedin"></i>
+        </div>
+        <div class="link-name">
+            LinkedIn
+        </div>
+    </a>
+    <a class="link" href="https://x.com/ogaspar__" target="_blank">
+        <div class="link-icon" style="background-color: #1DA1F2;">
+            <i class="fa-brands fa-x-twitter"></i>
+        </div>
+        <div class="link-name">
+            Twitter
+        </div>
+    </a>
+    <a class="link" href="https://www.instagram.com/gaspar.arthur_/?theme=dark" target="_blank">
+        <div class="link-icon" style="background-color: #E4405F;">
+            <i class="fa-brands fa-instagram"></i>
+        </div>
+        <div class="link-name">
+            Instagram
+        </div>
+    </a>
+`;
